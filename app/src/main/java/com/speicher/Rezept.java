d7a8a388a0e2cff8e3386a3dc302f9e58bb244f2
iluvater <ture.kuesener@gmx.de>
2016-07-22 20:04:22 +0200

    Test

---
+++ app/src/main/java/com/speicher/Rezept.java
@@ -0,0 +1,48 @@
+package com.speicher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ture on 22.07.2016.
+ */
+public class Rezept {
+    private String name;
+    private List<Zutat> zutaten;
+
+    public Rezept(String name){
+        zutaten = new ArrayList<Zutat>();
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Zutat> getZutaten() {
+        return zutaten;
+    }
+
+    public void setZutaten(List<Zutat> zutaten) {
+        this.zutaten = zutaten;
+    }
+
+    public void addZutat(Zutat z){
+        boolean found = false;
+        for(int i= 0;i<zutaten.size();i++) {
+            if(z.equals(zutaten.get(i))){
+                found = true;
+            }
+        }
+        if(!found){
+            zutaten.add(z);
+        }
+    }
+
+    public void deleteZutat(Zutat z){
+        zutaten.remove(z);
+    }
+}
---
+++ app/src/main/java/com/speicher/Zutat.java
@@ -0,0 +1,46 @@
+package com.speicher;
+
+/**
+ * Created by Ture on 22.07.2016.
+ */
+public class Zutat {
+    private double menge;
+    private String einheit;
+    private String name;
+
+    public Zutat(String name){
+        this.name =name;
+    }
+
+    public double getMenge() {
+        return menge;
+    }
+
+    public String getEinheit() {
+        return einheit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEinheit(String einheit) {
+        this.einheit = einheit;
+    }
+
+    public void setMenge(double menge) {
+        this.menge = menge;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEquals(Zutat z){
+        if(this.name.equals(z.getName())&&this.einheit.equals(z.getEinheit())&&this.menge==z.getMenge()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
