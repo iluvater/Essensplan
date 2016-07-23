5da126f36d9e523493336d7c4108c209d0a19e2c
iluvater <ture.kuesener@gmx.de>
2016-07-23 15:42:15 +0200

    Test

---
+++ app/src/main/java/com/speicher/LoadImpl.java
@@ -0,0 +1,17 @@
+package com.speicher;
+
+/**
+ * Created by Ture on 23.07.2016.
+ */
+public class LoadImpl implements LoadServ {
+
+    @Override
+    public Einkaufsplan getDataEinkaufsplan() {
+        return null;
+    }
+
+    @Override
+    public Essensplan getDataEssensplan() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/speicher/LoadServ.java
@@ -0,0 +1,9 @@
+package com.speicher;
+
+/**
+ * Created by Ture on 23.07.2016.
+ */
+public interface LoadServ {
+    public Essensplan getDataEssensplan();
+    public Einkaufsplan getDataEinkaufsplan();
+}
